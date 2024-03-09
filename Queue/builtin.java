54b1ead567def87af50ed8082975040cf2e842e2
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-03-09 11:28:01 +0530

    Queue builtin file change

--- Queue/builtin.java
+++ Queue/builtin.java
@@ -2,6 +2,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class builtin {
+
+        // Queue is framwork 
+        // we can not create object of framwork
+        // two class implement Queue -> LinkedList & ArrayDeque
+ 
     Queue<Integer> q = new LinkedList<>();
     
      Queue<Integer> q = new ArrayDeque<>();
