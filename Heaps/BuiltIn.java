64686c217f0a3d005c62687da6e845993ebc0a89
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-03-10 13:11:30 +0530

    Change in builtin file

--- Heaps/BuiltIn.java
+++ Heaps/BuiltIn.java
@@ -1,3 +1,9 @@
 PriorityQueue <Integer> pq = new PriorityQueue<>();
 
 PriorityQueue <Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());  // comparator->collection in java
+
+
+while(!pq.isEmpty()) {
+    System.out.println(pq.peek().name+" --> "+ pq.peek().rank);  // O(1)
+    pq.remove();  // O(logn)
+}
